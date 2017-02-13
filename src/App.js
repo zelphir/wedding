3c66a9b9e4d42e12117356f71a845dc27425fe9d
Roberto Manzella <zelphir@gmail.com>
2017-02-13 17:04:21 +0000

    wip

--- src/App.js
+++ src/App.js
@@ -1,28 +1,60 @@
 import React, { Component } from 'react'
-import ScrollableAnchor from 'react-scrollable-anchor'
-import { Button } from 'semantic-ui-react'
+import ScrollableAnchor, { goToAnchor } from 'react-scrollable-anchor'
+import { Sidebar, Segment, Button, Menu, Image, Icon, Header } from 'semantic-ui-react'
 import './App.css'
 
 class App extends Component {
-  render () {
+  state = { visible: false }
+  toggleVisibility = () => {
+    goToAnchor('section2', true)
+    this.setState({ visible: !this.state.visible })
+  }
+
+  render() {
+    const { visible } = this.state
     return (
       <div>
-        <a href='#section1'> Go to section 1 </a>
-        <a href='#section2'> Go to section 2 </a>
-        <ScrollableAnchor id={'section1'}>
-          <div className='App'>
-            Hello World
-            <Button>
-              Click Here
-            </Button>
-          </div>
-        </ScrollableAnchor>
-        <ScrollableAnchor id={'section2'}>
-          <div style={{height: '400px', backgroundColor: 'red'}}> How are you world? </div>
-        </ScrollableAnchor>
+        <Button onClick={this.toggleVisibility}>Toggle Visibility</Button>
+        <Sidebar.Pushable as={Segment}>
+          <Sidebar as={Menu} animation='uncover' width='thin' visible={visible} icon='labeled' vertical inverted>
+            <Menu.Item onClick={this.toggleVisibility} name='home'>Home</Menu.Item>
+            <Menu.Item onClick={this.toggleVisibility} name='test'>Test</Menu.Item>
+          </Sidebar>
+          <Sidebar.Pusher>
+            <ScrollableAnchor id={'section1'}>
+              <div className='App'>
+                Hello World
+              </div>
+            </ScrollableAnchor>
+            <ScrollableAnchor id={'section2'}>
+              <div style={{height: '400px', backgroundColor: 'red'}}> How are you world? </div>
+            </ScrollableAnchor>
+          </Sidebar.Pusher>
+        </Sidebar.Pushable>
       </div>
     )
   }
+  // render () {
+  //   return (
+  //     <div>
+  //       <Button onClick={this.toggleVisibility}>Toggle Visibility</Button>
+  //       <Sidebar.Pushable as={Segment}>
+  //         <Menu.Item href='#section1' name='home'>Home</Menu.Item>
+  //         <Menu.Item href='#section2' name='test'>Test</Menu.Item>
+  //       </Sidebar.Pushable>
+  //       <Sidebar.Pusher>
+  //         <ScrollableAnchor id={'section1'}>
+  //           <div className='App'>
+  //             Hello World
+  //           </div>
+  //         </ScrollableAnchor>
+  //         <ScrollableAnchor id={'section2'}>
+  //           <div style={{height: '400px', backgroundColor: 'red'}}> How are you world? </div>
+  //         </ScrollableAnchor>
+  //       </Sidebar.Pusher>
+  //     </div>
+  //   )
+  // }
 }
 
 export default App
