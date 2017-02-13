bafa3695a1b995f195a7051732f2a2e9e4e7bfaf
Roberto Manzella <zelphir@gmail.com>
2017-02-13 15:50:37 +0000

    Add semantic-ui

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import ScrollableAnchor from 'react-scrollable-anchor'
+import { Button } from 'semantic-ui-react'
 import './App.css'
 
 class App extends Component {
@@ -9,7 +10,12 @@ class App extends Component {
         <a href='#section1'> Go to section 1 </a>
         <a href='#section2'> Go to section 2 </a>
         <ScrollableAnchor id={'section1'}>
-          <div className='App'>Hello World</div>
+          <div className='App'>
+            Hello World
+            <Button>
+              Click Here
+            </Button>
+          </div>
         </ScrollableAnchor>
         <ScrollableAnchor id={'section2'}>
           <div style={{height: '400px', backgroundColor: 'red'}}> How are you world? </div>
